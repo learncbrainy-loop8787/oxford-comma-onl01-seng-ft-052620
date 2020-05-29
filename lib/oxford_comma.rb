2c287a5d5853c9a8322d954adc2b815012587b21
korey ko <koreykendrick1@gmail.com>
2020-05-29 16:35:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ def oxford_comma(array)
   if array.length == 1 
     array[0]
     elsif array.length == 2
-    array[0] = " and " + array[1]
-    elsif array.length > 2 
-    array.join(", ") 
+    
 end
 end   
   
